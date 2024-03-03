de105ac5321fa7b0203f5e39a6d64d0d3f92c66b
JeremyCheung17 <157504562+JeremyCheung17@users.noreply.github.com>
2024-03-03 21:36:06 +0000

    Your commit message here

--- src/hw2_main.c
+++ src/hw2_main.c
@@ -8,10 +8,39 @@
 #include <stdbool.h>
 #include <string.h>
 #include <unistd.h> 
+#include <getopt.h>
 
 int main(int argc, char **argv) {
     (void)argc;
     (void)argv;
+    const char *input_file = NULL;
+    const char *output_file = NULL;
+    int option;
+    while ((option = getopt(argc, argv, "i:o:c:p:r:")) != -1) {
+        switch (option) {
+            case 'i':
+                input_file = optarg;
+                break;
+            case 'o':
+                output_file = optarg;
+                break;
+            case 'c':
 
+                break;
+            case 'p':
+
+                break;
+            case 'r':
+
+                break;
+            case '?':
+            default:
+                return UNRECOGNIZED_ARGUMENT;
+        }
+    }
+    if (input_file == NULL || output_file == NULL) 
+    {
+        return MISSING_ARGUMENT;
+    }
     return 0;
 }
