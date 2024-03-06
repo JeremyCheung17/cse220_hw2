1848cf14c187fba53b11defec40b52cbda292dce
JeremyCheung17 <157504562+JeremyCheung17@users.noreply.github.com>
2024-03-06 00:12:45 +0000

    hi

--- src/hw2_main.c
+++ src/hw2_main.c
@@ -652,7 +652,6 @@ int main(int argc, char **argv) {
                 {
 
                 }
-                int num_colors = 0;
                 for (int i = 0; i < height; i++) 
                 {
                      for (int j = 0; j < width; j++) 
